b577cdb21ae45319af51b1900e5722159307100e
Biram Diouf <ibrahimabiram@gmail.com>
2020-12-12 22:46:54 +0000

    Calculate benefits while updating sale's status

--- app/Http/Controllers/SaleController.php
+++ app/Http/Controllers/SaleController.php
@@ -3,7 +3,9 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Aliment;
 use App\Models\Band;
+use App\Models\ExtraCharge;
 use App\Models\Sale;
 
 class SaleController extends Controller
@@ -93,14 +95,41 @@ class SaleController extends Controller
             'price' => 'required',
             'band' => 'required',
         ]);
+        $band_id = $request->get('band');
 
         $sale = Sale::find($id);
         $sale->quantity = $request->get('quantity');
         $sale->price = $request->get('price');
         $sale->band_id = $request->get('band');
         $sale->buyer = $request->get('buyer');
+        $sale->status = $request->get('status');
+
         $sale->total_price = $request->get('quantity') * $request->get('price');
         $sale->save();
+
+        if ($sale->status == "paid") {
+            $total_sales_price = 0;
+            $total_foods_price = 0;
+            $total_extras_price = 0;
+            $sales_band = (new Sale)->findSalesByBand($band_id);
+            $foods_band = (new Aliment)->findFoodsByBand($band_id);
+            $extras_band = (new ExtraCharge)->findExtrasByBand($band_id);
+            
+            foreach ($sales_band as $s) {
+                $total_sales_price += $s->total_price;
+            }
+            foreach ($foods_band as $food) {
+                $total_foods_price += $food->total_price;
+            }
+            foreach ($extras_band as $extra) {
+                $total_extras_price += $extra->total_price;
+            }
+
+            $band = (new Band)->findBand($band_id);
+            $band->benefits = $total_sales_price - ($band->purchase_price + $total_foods_price
+                                + $total_extras_price);
+            $band->save();
+        }
 
         return redirect()->route('sale.index')->with('message', 'Sale updated successfully!');
     }
