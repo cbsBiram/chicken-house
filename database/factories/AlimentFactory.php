58a8c2304531953f67d79d4b3d9a4a5946286a19
Biram Diouf <ibrahimabiram@gmail.com>
2020-12-14 07:17:47 +0000

    Add Factories for each model

---
+++ database/factories/AlimentFactory.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Aliment;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class AlimentFactory extends Factory
+{
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = Aliment::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            //
+        ];
+    }
+}
