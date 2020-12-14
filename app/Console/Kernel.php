5e880bdbb30f9de9f83eb929710900cc3505331d
Ibrahima Biram Diouf <ibrahimabiram@gmail.com>
2020-12-14 17:07:27 +0000

    Scheduler job

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -13,7 +13,7 @@ class Kernel extends ConsoleKernel
      * @var array
      */
     protected $commands = [
-        Commands\EveryFortnight::class,
+        Commands\DailyStatus::class,
     ];
 
     /**
@@ -24,7 +24,7 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        $schedule->command('fortnight:update')->daily();
+        $schedule->command('status:daily')->everyMinute();
     }
 
     /**
