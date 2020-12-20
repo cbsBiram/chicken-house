600f8adaf43e2e525b74ce223b7f8a931f9bdd3e
Ibrahima Biram Diouf <ibrahimabiram@gmail.com>
2020-12-20 01:37:13 +0000

    pgsql as database driver

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,6 +4,8 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
 
+use App\Models\User;
+
 class DatabaseSeeder extends Seeder
 {
     /**
@@ -14,11 +16,19 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         $this->call([
-        UserSeeder::class,
-        BandSeeder::class,
-        AlimentSeeder::class,
-        ExtraChargeSeeder::class,
-        SaleSeeder::class,
-    ]);
+            UserSeeder::class,
+            BandSeeder::class,
+            AlimentSeeder::class,
+            ExtraChargeSeeder::class,
+            SaleSeeder::class,
+        ]);
+        $admin = new User();
+        $admin->name = "admin";
+        $admin->email = "admin123@gmail.com";
+        $admin->password = bcrypt("adminpassword3");
+        $admin->email_verified_at = NOW();
+        $admin->phone = "034534494";
+        $admin->is_admin = 1;
+        $admin->save();
     }
 }
